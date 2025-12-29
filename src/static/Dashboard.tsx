27ac55edae7539d682a07c6503270f6eed74d7f7
Sri2103 <damasriharsha@gmail.com>
2025-12-29 09:33:28 +0530

    update image url

--- src/static/Dashboard.tsx
+++ src/static/Dashboard.tsx
@@ -1,10 +1,11 @@
 import Img from "../assets/dashboard.jpeg";
 
 export default function BiDashboard() {
+  const imgUrl = new URL(Img, import.meta.url).href;
   return (
     <div className="w-screen h-screen">
       <div className=" w-full h-full flex items-center justify-center ">
-        <img src={Img} alt="dashboard" />
+        <img src={imgUrl} alt="dashboard" />
       </div>
     </div>
   );
