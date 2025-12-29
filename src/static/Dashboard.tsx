6db0bad226999985fc796ea143b1c1a261366b9c
Sri2103 <damasriharsha@gmail.com>
2025-12-29 09:27:55 +0530

    update image

--- src/static/Dashboard.tsx
+++ src/static/Dashboard.tsx
@@ -1,10 +1,11 @@
+import Img from "../assets/dashboard.jpeg";
+
 export default function BiDashboard() {
   return (
     <div className="w-screen h-screen">
-      <div
-        className=" w-full h-full p-4  bg-center bg-no-repeat"
-        style={{ backgroundImage: `url(public/dashboard.jpeg)` }}
-      ></div>
+      <div className=" w-full h-full flex items-center justify-center ">
+        <img src={Img} alt="dashboard" />
+      </div>
     </div>
   );
 }
